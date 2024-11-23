c27e4466762d34e8db052ea6465615c9003880a4
flynnfrog <flynnfrog>
2024-11-23 22:13:08 +0100

    fix: user name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Create the user account
-groupadd --gid 1020 ubuntu
-useradd --shell /bin/bash --uid 1020 --gid 1020 --password $(openssl passwd ubuntu) --create-home --home-dir /home/ubuntu ubuntu
-usermod -aG sudo ubuntu
+groupadd --gid 1020 debian
+useradd --shell /bin/bash --uid 1020 --gid 1020 --password $(openssl passwd debian) --create-home --home-dir /home/debian debian
+usermod -aG sudo debian
 
 # Start xrdp sesman service
 /usr/sbin/xrdp-sesman
